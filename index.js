8dd0e1c1687a6e221903db395ed75d844d7af4e0
Zeyna Baaj <zainabaaj@gmail.com>
2023-05-25 12:27:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(jsonData => renderBooks(jsonData));
 }
 
 function renderBooks(books) {
@@ -12,6 +14,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
